3fda183b08ede71112ff9dd78f19d2ee065c6f1d
Henry Cooper <henrycooper@outlook.com>
2020-03-03 10:23:03 -0500

    Initial Commit

---
+++ StringEditorFramework/StringEditorFramework.h
@@ -0,0 +1,19 @@
+//
+//  StringEditorFramework.h
+//  StringEditorFramework
+//
+//  Created by Henry Cooper on 03/03/2020.
+//  Copyright © 2020 Henry Cooper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringEditorFramework.
+FOUNDATION_EXPORT double StringEditorFrameworkVersionNumber;
+
+//! Project version string for StringEditorFramework.
+FOUNDATION_EXPORT const unsigned char StringEditorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringEditorFramework/PublicHeader.h>
+
+
